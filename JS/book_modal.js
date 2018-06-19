ee9ded1cfacb086e4653654d2f08561e1ec13f9f
Jawan Higgins <jawanhiggins@jawans-mbp.attlocal.net>
2018-06-19 07:01:02 -0400

    Added Paypal functionality

--- JS/book_modal.js
+++ JS/book_modal.js
@@ -82,7 +82,7 @@ $(document).ready(function () {
 
             document.getElementById('dateId').innerHTML = '<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_blank">' +
                 '<input type="hidden" name="cmd" value="_s-xclick">' +
-                '<input type="hidden" name="hosted_button_id" value="DE5JHEDHV4TB2">' +
+                '<input type="hidden" name="hosted_button_id" value="PD97PNEUXKUKW">' +
                 '<input class="modal-order-btn" type="image" src="Style/img/Buy-Now-Btn.png" style="" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">' +
                 '</form>';
 
